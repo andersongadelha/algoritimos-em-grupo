75641eb46d33c295d18df481a4bb05d622af2d54
marcelooliveirazup <marcelo.oliveira@zup.com.br>
2024-12-10 12:02:36 -0300

    Implementando inputs de entrada de dados e um for

--- algoritimos/src/BubbleSort.java
+++ algoritimos/src/BubbleSort.java
@@ -1,7 +1,18 @@
+import javax.swing.*;
+
 public class BubbleSort {
 
     public static void main(String[] args) {
+        // Pergunta o tamanho da lista
+        int tamanho = Integer.parseInt(JOptionPane.showInputDialog("Digite o tamanho da lista: "));
 
+        // Cria o Array
+        int[] lista = new int[tamanho];
+
+        // Pergunta os itens da lista
+        for (int i = 0; i < tamanho; i++) {
+            lista[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite os itens da lista: "));
+        }
 
     }
 
