e8daa7c33ace5be2eba30eb0cf23104c7218c1a9
Anderson Lima <anderson.gadelha@zup.com.br>
2024-12-10 15:18:44 -0300

    (feat): adiciona testes para MergeSortUtil

--- algoritimos/src/Main.java
+++ algoritimos/src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Integer[] lista = {5, 2, 1 ,6, 7, 10};
+        Integer[] listaComUmElemento = {1};
+        Integer[] listaVazia = {};
 
         Integer[] listaOrdenada = MergeSortUtil.ordenar(lista);
+        Integer[] listaOrdenadaComUmElemento = MergeSortUtil.ordenar(listaComUmElemento);
+        Integer[] listaVaziaOrdenada = MergeSortUtil.ordenar(listaVazia);
+
+        System.out.println("Lista Original: " + Arrays.toString(lista));
+        System.out.println("Lista Ordenada: " + Arrays.toString(listaOrdenada));
+
+        System.out.println("Lista Original: " + Arrays.toString(listaComUmElemento));
+        System.out.println("Lista Ordenada: " + Arrays.toString(listaOrdenadaComUmElemento));
+
+        System.out.println("Lista Original: " + Arrays.toString(listaVazia));
+        System.out.println("Lista Ordenada: " + Arrays.toString(listaVaziaOrdenada));
+
     }
 }
